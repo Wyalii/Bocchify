843257686c3f699c9b4543729e0348646a7ac4d0
Giorgi Gigauri <gigaurigiorgi2727@gmail.com>
2025-01-29 23:13:04 +0400

    finished desing of landing page.

--- src/app/Components/RegistrationForm/registartion.component.ts
+++ src/app/Components/RegistrationForm/registartion.component.ts
@@ -1,8 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { ThemeService } from '../../services/theme.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'Registration-Form',
   templateUrl: 'registration.component.html',
   styleUrl: 'registration.component.scss',
+  imports: [CommonModule],
 })
-export class RegistrationForm {}
+export class RegistrationForm {
+  constructor(public themeService: ThemeService) {}
+}
