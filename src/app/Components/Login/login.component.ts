d99c16cbce4f69c2da54ca7a0f7d760ca9e6f8ad
Giorgi Gigauri <gigaurigiorgi2727@gmail.com>
2025-02-05 12:38:22 +0400

    login logic with token works.

--- src/app/Components/Login/login.component.ts
+++ src/app/Components/Login/login.component.ts
@@ -1,14 +1,41 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ThemeService } from '../../services/theme.service';
+import { CookieService } from 'ngx-cookie-service';
+
+interface AuthResponse {
+  message: string;
+  token: string;
+}
 
 @Component({
   selector: 'Login',
   templateUrl: 'login.component.html',
   styleUrl: 'login.component.scss',
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
 })
 export class Login {
-  login() {
-    console.log('login clicked');
+  constructor(
+    public themeService: ThemeService,
+    private http: HttpClient,
+    private cookieService: CookieService
+  ) {}
+
+  private apiUrl = 'http://localhost:5227/api/Auth/login';
+  login(username: string, password: string) {
+    const requestBody = { username, password };
+    this.http.post<AuthResponse>(this.apiUrl, requestBody).subscribe({
+      next: (response) => {
+        console.log('success:', response);
+        console.log(response.token);
+        if (response.token !== undefined && response.token !== null) {
+          this.cookieService.set('token', response.token, undefined, '/');
+        }
+      },
+      error: (response) => {
+        console.log('failed:', response);
+      },
+    });
   }
 }
--- src/app/Components/Register/register.component.ts
+++ src/app/Components/Register/register.component.ts
@@ -2,20 +2,18 @@ import { Component, EventEmitter, input, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThemeService } from '../../services/theme.service';
 import { animate, style, transition, trigger } from '@angular/animations';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'Register',
   templateUrl: 'register.component.html',
   styleUrl: 'register.component.scss',
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule],
 })
 export class Register {
-  private apiUrl = 'http://localhost:5227/api/Auth/register';
   constructor(public themeService: ThemeService, private http: HttpClient) {}
+  private apiUrl = 'http://localhost:5227/api/Auth/register';
   register(username: string, password: string) {
-    console.log(username);
-    console.log(password);
     const requestBody = { username, password };
     this.http.post(this.apiUrl, requestBody).subscribe({
       next: (response) => {
