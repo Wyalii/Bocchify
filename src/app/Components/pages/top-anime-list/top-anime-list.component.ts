95af29eb3309f1f810b923f42f96d537c17342e2
Giorgi Gigauri <gigaurigiorgi2727@gmail.com>
2025-04-01 09:35:48 +0400

    added pagination on top mangas and top animes routes.

--- src/app/Components/pages/top-anime-list/top-anime-list.component.ts
+++ src/app/Components/pages/top-anime-list/top-anime-list.component.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { JikanService } from '../../../services/jikan.service';
 import { Router } from '@angular/router';
 import { ThemeService } from '../../../services/theme.service';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-top-anime-list',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './top-anime-list.component.html',
   styleUrl: './top-anime-list.component.scss',
 })
@@ -14,6 +15,7 @@ export class TopAnimeListComponent implements OnInit {
   currentPage: number = 1;
   totalPages: number = 1;
   hasNextPage: boolean = false;
+  inputPage: number = 1;
   constructor(
     public jikanService: JikanService,
     private router: Router,
@@ -25,12 +27,36 @@ export class TopAnimeListComponent implements OnInit {
   }
   loadAnimes(page: number) {
     this.jikanService.getTopAnimes(page).subscribe((response) => {
+      console.log(response);
       this.topAnimes = response.data;
       this.currentPage = response.pagination.current_page;
       this.totalPages = response.pagination.last_visible_page;
       this.hasNextPage = response.pagination.has_next_page;
+      this.inputPage = this.currentPage;
     });
   }
+
+  goToPage() {
+    if (this.inputPage >= 1 && this.inputPage <= this.totalPages) {
+      this.loadAnimes(this.inputPage);
+    } else {
+      alert('Please enter a valid page number!');
+      this.inputPage = this.currentPage;
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.loadAnimes(this.currentPage - 1);
+    }
+  }
+
+  nextPage() {
+    if (this.hasNextPage) {
+      this.loadAnimes(this.currentPage + 1);
+    }
+  }
+
   navigateToAnimeDetails(AnimeId: number) {
     this.router.navigate(['/anime', AnimeId]);
   }
--- src/app/Components/pages/top-manga-list/top-manga-list.component.ts
+++ src/app/Components/pages/top-manga-list/top-manga-list.component.ts
@@ -3,10 +3,11 @@ import { JikanService } from '../../../services/jikan.service';
 import { ThemeService } from '../../../services/theme.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-top-manga-list',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './top-manga-list.component.html',
   styleUrl: './top-manga-list.component.scss',
 })
@@ -15,6 +16,7 @@ export class TopMangaListComponent {
   currentPage: number = 1;
   totalPages: number = 1;
   hasNextPage: boolean = false;
+  inputPage: number = 1;
   constructor(
     public jikanService: JikanService,
     private router: Router,
@@ -30,7 +32,29 @@ export class TopMangaListComponent {
       this.currentPage = response.pagination.current_page;
       this.totalPages = response.pagination.last_visible_page;
       this.hasNextPage = response.pagination.has_next_page;
+      this.inputPage = response.pagination.current_page;
     });
+  }
+
+  goToPage() {
+    if (this.inputPage >= 1 && this.inputPage <= this.totalPages) {
+      this.loadMangas(this.inputPage);
+    } else {
+      alert('Please enter a valid page number!');
+      this.inputPage = this.currentPage;
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.loadMangas(this.currentPage - 1);
+    }
+  }
+
+  nextPage() {
+    if (this.hasNextPage) {
+      this.loadMangas(this.currentPage + 1);
+    }
   }
   navigateToMangaDetails(MangaId: number) {
     this.router.navigate(['/manga', MangaId]);
