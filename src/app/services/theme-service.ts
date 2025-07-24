38af079014bf804aa5c6305b057f495e9b7db617
Giorgi Gigauri <gigaurigiorgi2727@gmail.com>
2025-07-24 23:44:50 +0400

    working on theme service and header...

---
+++ src/app/services/theme-service.ts
@@ -0,0 +1,62 @@
+import { computed, Injectable, signal } from '@angular/core';
+
+type ThemeMode = 'bocchi' | 'ryo' | 'ikuyo' | 'nikija';
+interface ThemeData {
+  backgroundColor: string;
+  backgroundImage: string;
+  backgroundImagePixel: string;
+  smallImage: string;
+  smallImagePixel: string;
+}
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeService {
+  userTheme = signal<ThemeMode>('bocchi');
+  pixelMode = signal<boolean>(false);
+
+  private themeMap: Record<ThemeMode, ThemeData> = {
+    bocchi: {
+      backgroundColor: '#f5a2b5',
+      backgroundImage: 'bocchi-landing.png',
+      backgroundImagePixel: 'bocchi-landing-pixel.png',
+      smallImage: 'bocchi.jpeg',
+      smallImagePixel: 'bocchi-pixel.png',
+    },
+    ryo: {
+      backgroundColor: '#0675c9',
+      backgroundImage: 'ryo-landing.png',
+      backgroundImagePixel: 'ryo-landing-pixel.png',
+      smallImage: 'ryo.jpg',
+      smallImagePixel: 'ryo-pixel.png',
+    },
+    ikuyo: {
+      backgroundColor: '#e80c05',
+      backgroundImage: 'ikuyo-landing.png',
+      backgroundImagePixel: 'ikuyo-landing-pixel.png',
+      smallImage: 'ikuyo.jpg',
+      smallImagePixel: 'ikuyo-pixel.png',
+    },
+    nikija: {
+      backgroundColor: '#e8e805',
+      backgroundImage: 'nikija-landing.png',
+      backgroundImagePixel: 'nikija-landing-pixel.png',
+      smallImage: 'nikija.jpg',
+      smallImagePixel: 'nikija-pixel.png',
+    },
+  };
+
+  currentMode = computed(() => this.userTheme());
+  theme = computed(() => this.themeMap[this.userTheme()]);
+
+  setTheme(newMode: ThemeMode) {
+    this.userTheme.set(newMode);
+  }
+
+  cycleTheme() {
+    const order: ThemeMode[] = ['bocchi', 'ryo', 'ikuyo', 'nikija'];
+    const currentIndex = order.indexOf(this.userTheme());
+    const nextMode = order[(currentIndex + 1) % order.length];
+    this.userTheme.set(nextMode);
+  }
+}
